a50082855e9d4364bb568586f14e0165eb9e358d
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-18 17:52:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,16 +3,16 @@ def new_hash
 end
 
 def my_hash
-  new_hash = {:programmer_name = "Elijah"};
+  new_hash = {:programmer_name => "Elijah"};
 end
 
 def pioneer
-  new_hash = {:name = "Grace Hopper"}
+  new_hash = {:name => "Grace Hopper"}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
-  hash = {:id = number}
+  hash = {:id => number}
   # return a hash with a key :id assigned to the provided number
 end
